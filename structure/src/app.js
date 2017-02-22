4c7ca58e5e145b9fe60bbcacc87e8107c98f85f2
YDJSZY <260211963@qq.com>
2017-02-22 18:17:03 +0800

    commit

--- structure/src/app.js
+++ structure/src/app.js
@@ -3,11 +3,12 @@
  */
 require(["config/require_config"],function () {
     require([
-        "../../kuaiyunapp/dist/scripts/garden.js",
+        "../../kuaiyunapp/dist/scripts/entrance.js",
         "./page_classes/template/template"
     ],function (garden,template) {
         var app = new garden.app("angularApp",["ngRoute","toaster","ui.select","uiSwitch","ngAnimate","ngSanitize"]);
         app.register(template.page(app.ky));
         app.start();
+        app.showLoadingImpl(true);
     })
 })
